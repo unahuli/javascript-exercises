6c118a9432214e449af6718e12092eb98459012d
unahuli <40204903+unahuli@users.noreply.github.com>
2025-06-18 23:45:35 +0800

    Completed exercise 01 - 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0) return 'ERROR';
+    let repeatedWord = '';
+    for(let i = 0; i < num; i++) {
+        repeatedWord += word;
+    }
+    return repeatedWord;
 };
 
 // Do not edit below this line
